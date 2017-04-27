eedd8e3e3dc94a7039a0c6267d7d73ac0bfdc358
Tim Smith <timothys740@gmail.com>
2017-04-27 17:16:25 -0400

    Moved Client files into client repository

---
+++ src/client/ClientApp.java
@@ -0,0 +1,24 @@
+package client;
+
+import utility.Protocol;
+
+/*
+ * Joseph Militello
+ * Logan Erexson
+ * Tim Smith
+ * Application File for Client
+ */
+
+public class ClientApp {
+	
+
+	public static void main(String[] args) {
+		
+		Client myClient = new Client(Protocol.SERVER_INFO,Protocol.DEFAULT_PORT);
+		
+		myClient.execute();
+
+
+	}
+
+}
